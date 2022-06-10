9793c7a5782f711d1710ff5f5e5b56a3c98291c6
David Konsumer <konsumer@jetboystudio.com>
2022-06-10 16:32:29 -0700

    fixing build

---
+++ src/Demo.jsx
@@ -0,0 +1,11 @@
+import { ImageInput, VideoInput } from 'media-mantine'
+
+export default function App () {
+  return (
+    <>
+      <ImageInput title='Single Image' />
+      <br />
+      <VideoInput title='Single Video' />
+    </>
+  )
+}
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// config for demo
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { URL } from 'url'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      'media-mantine': path.resolve(new URL('.', import.meta.url).pathname, './src/index.jsx')
+    }
+  }
+})
